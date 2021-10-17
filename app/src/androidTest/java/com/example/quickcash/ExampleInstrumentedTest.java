fd86684b2a0e33d00109411b8960beb0f40d3a36
Meet Kumar Patel <mt591517@dal.ca>
2021-10-17 05:12:42 -0300

    changed the expresso test again

--- app/src/androidTest/java/com/example/quickcash/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/quickcash/ExampleInstrumentedTest.java
@@ -25,7 +25,6 @@ import com.example.quickcash.UserManegement.SignUpActivity;
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
