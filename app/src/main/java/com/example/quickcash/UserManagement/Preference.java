2788d21ef12a2da0e6515131cdf6bba4578d38df
Raham Moghaddam <raham@Rahams-MacBook-Air.local>
2021-11-07 16:56:55 -0400

    Preference

---
+++ app/src/main/java/com/example/quickcash/UserManagement/Preference.java
@@ -0,0 +1,68 @@
+package com.example.quickcash.UserManagement;
+import java.util.UUID;
+
+
+public class Preference {
+
+    private String preferenceId;
+    private int jobType;
+    private int duration;
+
+    private int wage;
+    private String employeeName;
+
+
+
+    public Preference( int type, int duration, int wage,String employeeName) {
+        this.preferenceId = UUID.randomUUID().toString();
+        this.jobType = type;
+        this.duration = duration;
+
+        this.wage = wage;
+        this.employeeName = employeeName;
+    }
+
+    public Preference() {}
+
+    public String getPreferenceId() {
+        return preferenceId;
+    }
+
+    public void setPreferenceId(String preferenceId) {
+        this.preferenceId = preferenceId;
+    }
+
+
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+
+    public int getWage() {
+        return wage;
+    }
+
+    public void setWage(int wage) {
+        this.wage = wage;
+    }
+
+
+
+
+
+    public int getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(int jobType) {
+        this.jobType = jobType;
+    }
+
+
+
+}
