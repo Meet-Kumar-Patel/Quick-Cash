8a0a957a0d5f4ca79b50a97b74cf4a9dd8dfed1e
Syed-space1 <ab652165@dal.ca>
2021-11-07 16:53:48 -0400

    Completed Task to create Employer & Employee Home pages with buttons and the Tests for both pages plus the 2 wireframes for both pages

--- app/src/main/java/com/example/quickcash/Home/EmployerHomeActivity.java
+++ app/src/main/java/com/example/quickcash/Home/EmployerHomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.quickcash.Home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,8 +9,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.quickcash.R;
 import com.example.quickcash.UserManagement.SessionManager;
+import com.google.firebase.FirebaseApp;
 
 public class EmployerHomeActivity extends AppCompatActivity {
+    private Button createTasks;
+    private Button dashboard;
+
+
+    // Created intents for navigating to the pages
+    protected void navigateToCreateTasksPage() {
+        Intent createTasksPageIntent = new Intent(this, TaskListActivity.class);
+        startActivity(createTasksPageIntent);
+    }
+
+    protected void navigateToDashboardPage() {
+        Intent dashboardPageIntent = new Intent(this, test.class);
+        startActivity(dashboardPageIntent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +34,20 @@ public class EmployerHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_employer_home);
         SessionManager sessionManager = new SessionManager(getApplicationContext());
         sessionManager.checkLogin();
+
+        //Created 2 buttons: createTasks, dashboard and their respective event listeners to navigate to those pages
+        createTasks = (Button) findViewById(R.id.btnCreateTasksEmployerPage);
+        createTasks.setOnClickListener(view -> {
+            navigateToCreateTasksPage();
+        });
+
+        dashboard = (Button) findViewById(R.id.btnDashboardEmployerPage);
+        dashboard.setOnClickListener(view -> {
+            navigateToDashboardPage();
+        });
+
+        FirebaseApp.initializeApp(this);
+
         Button btnLogOut = findViewById(R.id.btnLogOutEmployer);
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +55,8 @@ public class EmployerHomeActivity extends AppCompatActivity {
                 sessionManager.logoutUser();
             }
         });
+
     }
 }
+
+
