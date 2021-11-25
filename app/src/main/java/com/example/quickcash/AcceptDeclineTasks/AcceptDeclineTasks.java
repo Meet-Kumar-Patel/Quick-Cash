a168418c0364a6daa29e69aa06d16e1fc2b02985
Syed-space1 <ab652165@dal.ca>
2021-11-24 20:57:09 -0400

    Created Branch

---
+++ app/src/main/java/com/example/quickcash/AcceptDeclineTasks/AcceptDeclineTasks.java
@@ -0,0 +1,102 @@
+package com.example.quickcash.AcceptDeclineTasks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.SearchView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quickcash.Home.EmployeeHomeActivity;
+import com.example.quickcash.JobPosting.JobPosting;
+import com.example.quickcash.R;
+import com.example.quickcash.TaskList.FirebaseTasks;
+import com.example.quickcash.UserManagement.MapsActivity;
+import com.example.quickcash.UserManagement.SessionManager;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class AcceptDeclineTasks extends AppCompatActivity {
+
+    RecyclerAdapter adapter;
+    ArrayList<JobPosting> jobPostingArrayList = new ArrayList<JobPosting>();
+    FirebaseDatabase db = FirebaseDatabase.getInstance("https://csci3130-quickcash-group9-default-rtdb.firebaseio.com/");
+    String city;
+    FirebaseTasks firebaseTasks = new FirebaseTasks();
+
+    private RecyclerView recyclerView;
+
+    //Refactoring needed, move search, and button init to new methods.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        city = intent.getStringExtra("City");
+        setContentView(R.layout.activity_task_list);
+        SessionManager sessionManager = new SessionManager(getApplicationContext());
+        recyclerView = findViewById(R.id.recyclerview);
+        firebaseTasks.getJobPostingsFromFirebase(this, city);
+        SearchView jobSearchView = findViewById(R.id.jobsearch);
+        jobSearchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        jobSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        Button resetButton = findViewById(R.id.resetbutton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jobSearchView.setQuery("", false);
+                jobSearchView.clearFocus();
+            }
+        });
+        Button homeButton = findViewById(R.id.homebutton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Change this to switch between employee and employer.
+                Intent homeIntent = new Intent(TaskListActivity.this, EmployeeHomeActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+    }
+
+    public void setAdapter(RecyclerAdapter adapter) {
+        this.adapter = adapter;
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    public RecyclerAdapter getAdapter() {
+        return adapter;
+    }
+
+    public void addJobPostingToArray(JobPosting jobPosting) {
+        jobPostingArrayList.add(jobPosting);
+    }
+
+    public ArrayList<JobPosting> getJobPostingArrayList() {
+        return jobPostingArrayList;
+    }
+}
