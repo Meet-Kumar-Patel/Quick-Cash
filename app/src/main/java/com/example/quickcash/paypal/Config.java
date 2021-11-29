2f2282ad8b139b6859d2976eef99ce4c335c1c77
Syed-space1 <ab652165@dal.ca>
2021-11-29 14:49:16 -0400

    Finished Paypal registration and received the client Id and added tothe config.java file and also created the paypalactivity.java file in the paypal folder

---
+++ app/src/main/java/com/example/quickcash/paypal/Config.java
@@ -0,0 +1,21 @@
+package com.example.quickcash.paypal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Config extends AppCompatActivity {
+
+    //Added our Client ID
+    public static final String PAYPAL_CLIENT_ID = "AVuMZDpIQGD3H-kAeKiledblq_119INklUL5ZUddRogrej3YavBIBPBdrgXF0c_ry5MEQziI_TMNCoH9";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+
+
+
+    }
+
