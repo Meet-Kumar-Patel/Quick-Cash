e2eec2597cb5155b321f2c9b8b6792be862f25eb
Syed-space1 <ab652165@dal.ca>
2021-11-29 18:35:05 -0400

    Made the layout for paypal invoice and the backend for paypal activity

--- app/src/main/java/com/example/quickcash/Paypal/PaypalActivity.java
+++ app/src/main/java/com/example/quickcash/Paypal/PaypalActivity.java
@@ -1,4 +1,5 @@
 package com.example.quickcash.Paypal;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -18,7 +19,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.quickcash.AcceptDeclineTasks.AcceptDeclineTasks;
+import com.example.quickcash.JobPosting.JobPosting;
+import com.example.quickcash.JobPosting.JobPostingActivity;
 import com.example.quickcash.R;
+import com.example.quickcash.UserManagement.preferencePage;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PayPalService;
@@ -30,24 +40,29 @@ import org.json.JSONObject;
 
 import java.math.BigDecimal;
 
-
 public class PaypalActivity extends AppCompatActivity {
 
-    //@Override
-    //protected void onCreate(Bundle savedInstanceState) {
-        //super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_paypal);
-
+    String username;
 
     ActivityResultLauncher activityResultLauncher;
 
     private static final int PAYPAL_REQUEST_CODE = 555;
     private static PayPalConfiguration config;
-    Button btnPayNow;
-    EditText edtAmount;
-    TextView paymentTV;
 
+    Button btnPayNow;
+    Button btnGoBack;
+    TextView txtName;
+    TextView txtWage;
+    TextView txtDuration;
+    TextView txtTotal;
+    TextView txtError;
     String amount = "";
+
+
+    protected void navigateToAcceptDeclinePage() {
+        Intent navToAcceptDecline = new Intent(this, AcceptDeclineTasks.class);
+        startActivity(navToAcceptDecline);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,10 +72,25 @@ public class PaypalActivity extends AppCompatActivity {
                 .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
                 .clientId(Config.PAYPAL_CLIENT_ID);
 
-        edtAmount = findViewById(R.id.etAmount);
+        btnGoBack = (Button) findViewById(R.id.btnGoBack);
+        btnGoBack.setOnClickListener(view -> {
+            navigateToAcceptDeclinePage();
+        });
+
+        Intent intent = getIntent();
+
+        String jobID = intent.getStringExtra("jobID");
+        username = intent.getStringExtra("username");
+
         btnPayNow = findViewById(R.id.btnPayNow);
-        // creating a variable for button, edit text and status tv.
-        paymentTV = findViewById(R.id.idTVStatus);
+        btnGoBack = findViewById(R.id.btnGoBack);
+        txtName = findViewById(R.id.txtNameValue);
+        txtDuration = findViewById(R.id.txtDurationValue);
+        txtTotal = findViewById(R.id.txtTotalValue);
+        txtError = findViewById(R.id.invoiceErrorMessage);
+
+        retrieveDataFromFirebase(jobID);
+
         // initializing Activity Launcher
         initializeActivityLauncher();
 
@@ -69,7 +99,58 @@ public class PaypalActivity extends AppCompatActivity {
             public void onClick(View v) {
                 processPayment();
             }
+
         });
+
+    }
+    protected void retrieveDataFromFirebase(String id) {
+        DatabaseReference jpDatabase = FirebaseDatabase.getInstance("https://csci3130-quickcash-group9-default-rtdb.firebaseio.com/").getReference(JobPosting.class.getSimpleName());
+        jpDatabase.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // When the data is received, verify the user credential
+                if (dataSnapshot.exists()) {
+
+                    try {
+                        getJPbyID(dataSnapshot, id);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Could retrieve: " + error.getCode());
+            }
+
+        });
+    }
+JobPosting jobPostingOBJ;
+    protected JobPosting getJPbyID(DataSnapshot dataSnapshot, String id) throws Exception {
+        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+            JobPosting jobPosting = snapshot.getValue(JobPosting.class);
+            boolean idMatches = jobPosting.getJobPostingId().equals(id);
+            if (idMatches) {
+                populateLayout(jobPosting);
+                jobPostingOBJ = jobPosting;
+                return jobPosting;
+            }
+        }
+        return null;
+    }
+
+    protected void populateLayout(JobPosting jobPosting) {
+        // Get the layout views
+
+
+        txtName.setText(username);
+        txtDuration.setText(jobPosting.getDuration());
+        txtWage.setText(jobPosting.getWage());
+        amount = String.valueOf(jobPosting.getDuration() * jobPosting.getWage());
+        txtTotal.setText(amount);
+
     }
 
     private void initializeActivityLauncher() {
@@ -87,7 +168,7 @@ public class PaypalActivity extends AppCompatActivity {
                             JSONObject payObj = new JSONObject(paymentDetails);
                             String payID = payObj.getJSONObject("response").getString("id");
                             String state = payObj.getJSONObject("response").getString("state");
-                            paymentTV.setText("Payment " + state + "\n with payment id is " + payID);
+                            txtError.setText("Payment " + state + "\n with payment id is " + payID);
                         } catch (JSONException e) {
                             // handling json exception on below line
                             Log.e("Error", "an extremely unlikely failure occurred: ", e);
@@ -105,8 +186,6 @@ public class PaypalActivity extends AppCompatActivity {
     }
 
     private void processPayment() {
-        // Getting amount from editText
-        amount = edtAmount.getText().toString();
         // Creating Paypal payment
         PayPalPayment payPalPayment = new PayPalPayment(new BigDecimal(String.valueOf(amount)),"CAD","Purchase Goods",PayPalPayment.PAYMENT_INTENT_SALE);
         // Creating Paypal Payment activity intent
