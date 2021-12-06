0b0ada961e94ca41f684b9ab5a655495f33b5ff5
Jobenpreet Singh <jb582801@dal.ca>
2021-12-05 22:24:15 -0400

    Hotfix

--- app/src/main/java/com/example/quickcash/paypal/Config.java
+++ app/src/main/java/com/example/quickcash/paypal/Config.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Config extends AppCompatActivity {
 
     //Added our Client ID
-    public static final String PAYPAL_CLIENT_ID = "AVuMZDpIQGD3H-kAeKiledblq_119INklUL5ZUddRogrej3YavBIBPBdrgXF0c_ry5MEQziI_TMNCoH9";
+    public static final String PAYPAL_CLIENT_ID = "AVeHA2bqxWEN6g4wn1b3e7kDgc8hsdp1oaLQ9N3Yy00mbjHIPCDrR-4eDJ0FlvOOVR85rkP5hBUOInao";
 
     }
 
